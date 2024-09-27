7a6d6ccebeb5707e9ee831e372a51fe74bfce75d
Jurif <zujishikizu@gmail.com>
2024-09-27 02:03:38 +0200

    Added some meshes, particles and actors LillyPads, JFHud, Teleporter and EmptyLillyPad.
    Added actors LillyPads, JFHud, Teleporter and EmptyLillyPad.
    Added meshes for lillypads, sky, water, lilly flowers.
    Added particles and lillypads datatable.
    Added the JumpyFrogsMap map file.
    Added WoodSign.

--- Source/JumpyFrogs/Private/Actors/LilyPads.cpp
+++ Source/JumpyFrogs/Private/Actors/LilyPads.cpp
@@ -1,14 +1,38 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Actors/LilyPads.h"
 
 // Sets default values
 ALilyPads::ALilyPads()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> LilyPadsMeshObj;
+		ConstructorHelpers::FObjectFinderOptional<UMaterial> LilyPadGreenMat;
+		ConstructorHelpers::FObjectFinderOptional<UDataTable> LillyPadsDataTable_DT;
+		//ConstructorHelpers::FObjectFinderOptional<UDataTable> ScoringDataTable_BP;
+		FConstructorStatics()
+			: LilyPadsMeshObj(TEXT("/Game/LilyPads/StaticMesh/LP_01"))
+			, LilyPadGreenMat(TEXT("/Game/LilyPads/Materials/WaterLily_Pad_Green"))
+			, LillyPadsDataTable_DT(TEXT("/Game/DataTables/LillyPadsMeshDataT.LillyPadsMeshDataT"))
+			//, ScoringDataTable_BP(TEXT("/Game/DataTables/DataTableFloatMultiplier.DataTableFloatMultiplier"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
 
+	//UDataTable* LillyPadsDataTable;
+	LillyPadsDataTable = ConstructorStatics.LillyPadsDataTable_DT.Get();
+	//ScoringDataTable = ConstructorStatics.ScoringDataTable_BP.Get();
+	// Create static mesh component
+	LilyPadsMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LilyPadsMesh"));
+	RootComponent = LilyPadsMesh;
+	LilyPadsMesh->SetStaticMesh(ConstructorStatics.LilyPadsMeshObj.Get());
+	LilyPadsMesh->SetRelativeScale3D(FVector(1.f, 1.f, 1.f));
+	LilyPadsMesh->SetRelativeLocation(FVector(8.0f, 8.0f, 200.0f));
+	LilyPadsMesh->SetMaterial(0, ConstructorStatics.LilyPadGreenMat.Get());
+	//LilyPadsMesh->SetupAttachment(DummyRoot);//AttachTo(DummyRoot);
+	LilyPadsMesh->bCastDynamicShadow = false;
 }
 
 // Called when the game starts or when spawned
@@ -17,11 +41,20 @@ void ALilyPads::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void ALilyPads::Tick(float DeltaTime)
+void ALilyPads::ReApplyStaticMesh(int32 WhichMesh)
 {
-	Super::Tick(DeltaTime);
+	UStaticMesh* NewMesh;
+	TArray<FLillyPadsData*> LillyPadsRowList;
+	//TArray<FStatScoringData*> StatScoringRowList;
 
+	//	ScoringDataTable->GetAllRows<FStatScoringData>("Some Debug Message String if Fails", StatScoringRowList);
+	////uncomment31fs:GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Blue, FString::SanitizeFloat(StatScoringRowList[3]->Multiplier));
+	LillyPadsDataTable->GetAllRows<FLillyPadsData>("Some Debug Message String if Failes", LillyPadsRowList);
+
+	const TCHAR* chars = *LillyPadsRowList[WhichMesh - 1]->LillyPadMeshAssetLink;
+	//const TCHAR* chars = *LilyPadsList[WhichMesh];
+	NewMesh = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), NULL, chars));
+
+	LilyPadsMesh->SetStaticMesh(NewMesh);
+	this->SetActorEnableCollision(false);
 }
-
--- Source/JumpyFrogs/Public/Actors/LilyPads.h
+++ Source/JumpyFrogs/Public/Actors/LilyPads.h
@@ -19,8 +19,99 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	void ReApplyStaticMesh(int32 WhichMesh);
 
+	/** StaticMesh component */
+	UPROPERTY(Category = Block, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* LilyPadsMesh;
+
+public:	
+	UDataTable* LillyPadsDataTable;
+};
+
+USTRUCT(BlueprintType)
+struct FLillyPadsData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	/*
+	FLillyPadsData()
+	: LillyPadMeshAssetLink(0)
+	{}*/
+
+	// The 'Name' column is the same as the XP Level 
+
+	//XP to get to the given level from the previous level
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+	FString LillyPadMeshAssetLink;
+};
+//
+//USTRUCT()
+//struct FAiSolverList : public FTableRowBase
+//{
+//	GENERATED_USTRUCT_BODY()
+//
+//public:
+//
+//	FAiSolverList()
+//		: AiOrder({ 0 })
+//		, AiLevel({ FVector2D(0.0f,0.0f), FVector2D(0.0f,0.0f) })
+//	{}
+//
+//	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Ai Solver")
+//	TArray<int32> AiOrder;
+//	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Ai Solver")
+//	TArray<FVector2D> AiLevel;
+//	//FVector2D AiLevel;
+//	// FVector2D TestVector;
+//	//int32 IntegerTest;
+//	//TArray<FVector2D> AiOrder;
+//
+//};
+USTRUCT()
+struct FLevelsDataList : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	FLevelsDataList()
+		: FrogsList(0)
+		, LilyPadsMeshNumList(0)
+		, DistanceList(2048.0f)
+		, CamLoc(FVector(0.0f, 0.0f, 0.0f))
+		, Level({ FVector2D(0.0f,0.0f) })
+	{}
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	int32 FrogsList;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	int32 LilyPadsMeshNumList;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	float DistanceList;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	FVector CamLoc;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	TArray<FVector2D> Level;
+};
+USTRUCT()
+struct FTelAndBombsDataList : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	FTelAndBombsDataList()
+		: NumOfBombsList(0)
+		, TelDataList({ 0.0f })
+		, TeleportersList({ FVector(0.0f,0.0f,0.0f) })
+	{}
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	int32 NumOfBombsList;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	TArray<float> TelDataList;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnLevels")
+	TArray<FVector> TeleportersList;
 };
